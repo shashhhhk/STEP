0ed1a5b3046778f970abdb6aac8b77310aea396d
Shashank kk <shashank.prasad.cr7@gmail.com>
2025-04-16 14:58:50 +0530

    EDITED

--- Java Add On Qns/RightNumber.java
+++ Java Add On Qns/RightNumber.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 class RightNumber{
     public static void structure(int rows){
         for(int i=0;i<rows;i++){
-            int k=1;
-            for(int j=0;j<i+1;j++){
-                System.out.print(k+"");
+            int l=1;
+            for(int j=0;j<rows-i-1;j++)System.out.print(" ");
+            for(int k=0;k<2*i+1;k++){
+                System.out.print(l+" ");
                 k++;
             }
             System.out.println();
